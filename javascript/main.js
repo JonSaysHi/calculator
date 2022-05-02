eed15449c65c4e04d47c5c5a1f533cc3b1188cb7
Jon Hii <jonathan.hii@outlook.com>
2022-05-03 00:55:51 +1000

    Created functions for each button, with each number button adding to an array

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,25 @@
+let resultDisplay = [];
+let x;
+let y;
+const clearButton = document.querySelector("#clear");
+const plusOrMinusButton = document.querySelector("#plusminus");
+const percentButton = document.querySelector("#percent");
+const divideButton = document.querySelector("#divide");
+const multiplyButton = document.querySelector("#multiply");
+const subtractButton = document.querySelector("#subtract");
+const addButton = document.querySelector("#add");
+const equalsButton = document.querySelector("#equals");
+const periodButton = document.querySelector("#period");
+const one = document.querySelector("#one");
+const two = document.querySelector("#two");
+const three = document.querySelector("#three");
+const four = document.querySelector("#four");
+const five = document.querySelector("#five");
+const six = document.querySelector("#six");
+const seven = document.querySelector("#seven");
+const eight = document.querySelector("#eight");
+const nine = document.querySelector("#nine");
+
 const add = function (x, y) {
   return x + y;
 };
@@ -27,3 +49,41 @@ const operate = function (operator, x, y) {
     return "ERROR";
   }
 };
+
+let displayValue = function () {};
+
+one.addEventListener("click", () => {
+  resultDisplay.push(1);
+});
+
+two.addEventListener("click", () => {
+  resultDisplay.push(2);
+});
+
+three.addEventListener("click", () => {
+  resultDisplay.push(3);
+});
+
+four.addEventListener("click", () => {
+  resultDisplay.push(4);
+});
+
+five.addEventListener("click", () => {
+  resultDisplay.push(5);
+});
+
+six.addEventListener("click", () => {
+  resultDisplay.push(6);
+});
+
+six.addEventListener("click", () => {
+  resultDisplay.push(7);
+});
+
+six.addEventListener("click", () => {
+  resultDisplay.push(8);
+});
+
+six.addEventListener("click", () => {
+  resultDisplay.push(9);
+});
