4a38d09ae853911a444381205e9ed9262d94e77d
Jon Hii <jonathan.hii@outlook.com>
2022-05-02 19:49:04 +1000

    Created operator function

--- javascript/main.js
+++ javascript/main.js
@@ -13,3 +13,17 @@ const multiply = function (x, y) {
 const divide = function (x, y) {
   return x / y;
 };
+
+const operate = function (operator, x, y) {
+  if (operator === "add") {
+    add(x, y);
+  } else if (operator === "subtract") {
+    subtract(x, y);
+  } else if (operator === "multiply") {
+    multiply(x, y);
+  } else if (operator === "divide") {
+    divide(x, y);
+  } else {
+    return "ERROR";
+  }
+};
